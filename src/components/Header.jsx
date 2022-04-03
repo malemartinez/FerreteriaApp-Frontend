5fefea705335742372ea06549d8a2b1a06fcda59
Alejandra Martinez <martinezmayra605@gmail.com>
2022-04-03 11:56:06 -0500

    adicion de metodo para cerrar sesion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
+import { signOut , getAuth } from 'firebase/auth';
+import app from '../firebase-config';
+import { useDispatch } from 'react-redux';
 import {
-  NavLink
+  NavLink, useNavigate
 } from "react-router-dom";
+import { setCloseSesion } from '../redux/registroDuck';
 
+const auth = getAuth(app);
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const LogOut = () => {
+    signOut(auth);
+    dispatch(setCloseSesion());
+    navigate("/")
+}
   return ( 
 
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-dark bg-warning">
       <h1>FerreteriaApp</h1>
-      <div className="container-md justify-content-end">
+      <div className="container-lg justify-content-end">
         {/* <NavLink className="navbar-brand" to="/">Inicio</NavLink> */}
         <NavLink className="navbar-brand" to="/admin/volante">Volante</NavLink>
         <NavLink className="navbar-brand " to="/admin/Inventario" >Inventario</NavLink>
         <NavLink className="navbar-brand " to="/admin/registroVenta" >Registrar Venta</NavLink>
+        <button  className= "navbar-brand btn btn-md btn-warning" onClick={() => LogOut()}>Cerrar sesión</button>
       </div>
     </nav>
 
